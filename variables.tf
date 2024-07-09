842ba7a8731e9337fee6ad96b9e797b66577a551
Hamza <ihamza59@icloud.com>
2024-07-09 14:07:23 +0500

    fixed variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "location" {
-  description = "West US"
+  default = "West US"
   type        = string
 }
 
 variable "RG_name" {
-  description = "1-b94aa0d4-playground-sandbox"
+  default = "1-b94aa0d4-playground-sandbox"
   type        = string
 }
